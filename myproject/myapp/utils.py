165df463f4f4cd5b67f2677a085890a98a4e8e40
Sivakanesh <sksivakanesh12@gmail.com>
2024-03-16 01:26:27 +0530

    credents

--- myproject/myapp/utils.py
+++ myproject/myapp/utils.py
@@ -3,9 +3,9 @@ from django.conf import settings
 
 def send_sms_message(to_number, message):
     # Your Twilio credentials
-    account_sid = settings.TWILIO_ACCOUNT_SID
-    auth_token = settings.TWILIO_AUTH_TOKEN
-    twilio_phone_number = settings.TWILIO_PHONE_NUMBER
+    account_sid = settings.TWILIO_ACCOUNTSID
+    auth_token = settings.TWILIO_AUTHTOKEN
+    twilio_phone_number = settings.TWILIO_PHONENUMBER
 
     # Initialize Twilio client
     client = Client(account_sid, auth_token)

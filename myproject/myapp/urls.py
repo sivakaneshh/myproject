c5bf253a904989d3f76f707fb8e5f1cca683542c
Sivakanesh <sksivakanesh12@gmail.com>
2024-03-15 09:30:15 +0530

    empty

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,8 +1,7 @@
 # myapp/urls.py
 
 from django.urls import path
-from .views import index, user_login,maincanteen,misccanteen,signup,user_logout,checkout,conformation
-
+from .views import index, user_login,maincanteen,misccanteen,signup,user_logout,checkout
 urlpatterns = [
     path('', index, name='index'),
     path('login.html/', user_login, name='login'),
@@ -12,5 +11,4 @@ urlpatterns = [
     path('signup.html/', signup, name='signup'),
     path('logout.html/', user_logout, name="logout"),
     path('checkout.html/', checkout, name="checkout"),
-    path('conformation.html',conformation, name='conformation')
 ]

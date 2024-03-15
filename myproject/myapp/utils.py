4eca04fcd5d34f16b83bf58bd36a07f21b04277a
Sivakanesh <sksivakanesh12@gmail.com>
2024-03-16 01:29:58 +0530

    rem

--- myproject/myapp/utils.py
+++ myproject/myapp/utils.py
@@ -3,20 +3,20 @@ from django.conf import settings
 
 def send_sms_message(to_number, message):
     # Your Twilio credentials
-    account_sid = settings.TWILIO_ACCOUNTSID
-    auth_token = settings.TWILIO_AUTHTOKEN
-    twilio_phone_number = settings.TWILIO_PHONENUMBER
+    #account_sid = settings.TWILIO_ACCOUNTSID
+    #auth_token = settings.TWILIO_AUTHTOKEN
+    #twilio_phone_number = settings.TWILIO_PHONENUMBER
 
     # Initialize Twilio client
-    client = Client(account_sid, auth_token)
+    #client = Client(account_sid, auth_token)
 
-    try:
+    #try:
         # Send SMS message using Twilio
-        client.messages.create(
-            to=to_number,
-            from_=twilio_phone_number,
-            body=message
-        )
-    except Exception as e:
+       # client.messages.create(
+        #    to=to_number,
+        #    from_=twilio_phone_number,
+        #    body=message
+       # )
+    #except Exception as e:
         # Handle any errors that occur during message sending
-        print(f"Error sending SMS: {str(e)}")
+     #   print(f"Error sending SMS: {str(e)}")
